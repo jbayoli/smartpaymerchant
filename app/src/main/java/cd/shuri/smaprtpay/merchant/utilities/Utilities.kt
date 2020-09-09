356a0662e193cc04a6b190ad09f762a07a27e797
Judicael Bayoli <bayoli.j@infoset.cd>
2020-09-09 14:25:23 +0100

    Add card validation

--- app/src/main/java/cd/shuri/smaprtpay/merchant/utilities/Utilities.kt
+++ app/src/main/java/cd/shuri/smaprtpay/merchant/utilities/Utilities.kt
@@ -17,3 +17,25 @@ fun phoneNumberFormat(phone: String): String {
     }
     return phoneNumberFormat
 }
+
+fun validateCreditCardNumber(str: String): Boolean {
+    val ints = IntArray(str.length)
+    for (i in str.indices) {
+        ints[i] = str.substring(i, i + 1).toInt()
+    }
+    var i = ints.size - 2
+    while (i >= 0) {
+        var j = ints[i]
+        j *= 2
+        if (j > 9) {
+            j = j % 10 + 1
+        }
+        ints[i] = j
+        i -= 2
+    }
+    var sum = 0
+    for (index in ints.indices) {
+        sum += ints[index]
+    }
+    return sum % 10 == 0
+}
