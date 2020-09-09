43c3511a763b113a59ccb520748d978801557a32
Judicael Bayoli <bayoli.j@infoset.cd>
2020-09-09 14:33:34 +0100

    Send code request in accountViewModel

--- app/src/main/java/cd/shuri/smaprtpay/merchant/screens/account/AccountViewModel.kt
+++ app/src/main/java/cd/shuri/smaprtpay/merchant/screens/account/AccountViewModel.kt
@@ -3,6 +3,14 @@ package cd.shuri.smaprtpay.merchant.screens.account
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import cd.shuri.smaprtpay.merchant.network.CodeRequest
+import cd.shuri.smaprtpay.merchant.network.SmartPayApi
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class AccountViewModel: ViewModel() {
@@ -16,13 +24,55 @@ class AccountViewModel: ViewModel() {
     private val _navigateToValidationFragment  = MutableLiveData<Boolean>()
     val navigateToValidationFragment : LiveData<Boolean> get() = _navigateToValidationFragment
 
+    private val _showDialogLoader = MutableLiveData<Boolean>()
+    val showDialogLoader: LiveData<Boolean> get() = _showDialogLoader
+
+    private val _showTToastForError = MutableLiveData<Boolean>()
+    val showTToastForError: LiveData<Boolean> get() = _showTToastForError
+
+    private var token = ""
+
+    private var countryCode = ""
+
+    private var viewModelJob = Job()
+
+    private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
     init {
         _isPhoneNumberCorrect.value = true
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Timber.e("getInstanceId failed ${task.exception}")
+                    return@OnCompleteListener
+                }
+                // Get new Instance ID token
+                token = task.result?.token!!
+                // Log and toast
+                Timber.d("token : $token")
+            })
     }
 
     fun setPhoneNumber(phoneNumber : String) {
         _phoneNumber.value = phoneNumber
         checkPhoneNumberField()
+    }
+
+    private fun sendCode(phoneNumber: String) {
+        viewModelScope.launch {
+            try {
+                _showDialogLoader.value = true
+                val request = CodeRequest(phoneNumber, token)
+                val result = SmartPayApi.smartPayApiService.sendCodeAsync(request).await()
+                Timber.d("message: ${result.message} status: ${result.status}")
+                _showDialogLoader.value = false
+                _navigateToValidationFragment.value = true
+            } catch (e: Exception) {
+                Timber.e("$e")
+                _showDialogLoader.value = false
+                _showTToastForError.value = true
+            }
+        }
     }
 
     private fun checkPhoneNumberField() {
@@ -34,7 +84,7 @@ class AccountViewModel: ViewModel() {
             else {
                 Timber.d("Ok")
                 _isPhoneNumberCorrect.value = true
-                _navigateToValidationFragment.value = true
+                sendCode("$countryCode${phoneNumber.value}")
             }
         }
     }
