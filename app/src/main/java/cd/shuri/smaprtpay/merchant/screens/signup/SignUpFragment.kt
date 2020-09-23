35fd6ca9b962c9f049210237e021188e61d3daaa
Judicael Bayoli <bayoli.j@infoset.cd>
2020-09-23 15:03:39 +0100

    listning to merchant code to update username

--- app/src/main/java/cd/shuri/smaprtpay/merchant/screens/signup/SignUpFragment.kt
+++ app/src/main/java/cd/shuri/smaprtpay/merchant/screens/signup/SignUpFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -13,6 +14,7 @@ import androidx.navigation.fragment.findNavController
 import cd.shuri.smaprtpay.merchant.databinding.FragmentSignUpBinding
 import cd.shuri.smaprtpay.merchant.network.SingUpRequest
 import cd.shuri.smaprtpay.merchant.utilities.LoaderDialog
+import java.util.*
 
 /**
  * A simple [Fragment] subclass.
@@ -47,6 +49,10 @@ class SignUpFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.validateButton.setOnClickListener {
             signUp()
+        }
+
+        binding.merchantCodeTet.addTextChangedListener {
+            binding.userNameTet.setText(it.toString().toLowerCase(Locale.ROOT))
         }
     }
 
